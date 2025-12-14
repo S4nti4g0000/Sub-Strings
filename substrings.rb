cab5843f0e32c67aff78ae4e218e49c31e0c9067
Santiago <98556731+S4nti4g0000@users.noreply.github.com>
2025-12-14 13:38:21 -0500

    Add reduce method to count words
    A basic word-counting approach was added. This uses the reduce method on
    the temporary array to create and return a hash that tracks how many
    times each word appears in the dictionary. Within the reduce block, each
    word is checked against the dictionary using the include? method. If the
    word exists, its corresponding key in the hash is incremented by 1.

--- substrings.rb
+++ substrings.rb
@@ -1,7 +1,14 @@
 DICTIONARY = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit","below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-def substring(string)
+def substring(string, dictionary)
   temp = string.downcase
-  temp.delete!(",!?'") 
-  temp = temp.split(" ")
+  temp = temp.delete!(",!?'").split(" ")
+  
+
+  temp.reduce(Hash.new(0)) do |amount,word|
+    amount[word] += 1 if dictionary.include?(word)
+    amount
+  end
+
 end
+
