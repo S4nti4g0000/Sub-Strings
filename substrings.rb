fc091c6dc464671f07806077be63c627c41890f3
Santiago <98556731+S4nti4g0000@users.noreply.github.com>
2025-12-16 19:24:53 -0500

    Add scan method to find words
    To make the code simpler, the scan method is now used with a regexp so
    all the words are found and then counted by the reduce method

--- substrings.rb
+++ substrings.rb
@@ -1,14 +1,10 @@
-DICTIONARY = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit","below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+DICTIONARY = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substring(string, dictionary)
-  temp = string.downcase
-  temp = temp.delete!(",!?'").split(" ")
-  
+  temp = string.downcase.scan(/\w+/)
 
   temp.reduce(Hash.new(0)) do |amount,word|
     amount[word] += 1 if dictionary.include?(word)
     amount
   end
-
 end
-
